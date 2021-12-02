cb4ee587c0fd5ba5f19c4732cbfc7264c1f1fb31
sinhaashish <ashi.sinha.87@gmail.com>
2021-12-02 13:25:31 +0530

    1. Two Sum

--- leetcode/1.go
+++ leetcode/1.go
@@ -1,3 +1,12 @@
+//Given an array of integers nums and an integer target, return indices of
+// the two numbers such that they add up to target.
+//You may assume that each input would have exactly one solution,
+//and you may not use the same element twice. You can return the answer in any order.
+
+//Input: nums = [2,7,11,15], target = 9
+//Output: [0,1]
+//Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+
 package main
 
 import "fmt"
@@ -10,14 +19,15 @@ func main() {
 
 func twoSum(nums []int, target int) []int {
 	result := make([]int, 0, 2)
-	map1 := make(map[int]int)
+	hashMap := make(map[int]int)
+
 	for i, k := range nums {
-		if containsKey(map1, target-k) {
+		if containsKey(hashMap, target-k) {
 			result = append(result, i)
-			result = append(result, map1[target-k])
+			result = append(result, hashMap[target-k])
 			return result
 		}
-		map1[k] = i
+		hashMap[k] = i
 	}
 	return result
 }
